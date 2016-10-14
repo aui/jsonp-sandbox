efd85d3f968e200852e533bcbe762a9d13a15227
糖饼 <1987.tangbin@gmail.com>
2016-10-14 16:39:50 +0800

    更新测试用例

--- test/xss.js
+++ test/xss.js
@@ -1,7 +1,69 @@
-jsonp_callback({
-	name: '糖饼',
-	weibo: 'http://weibo.com/planeart'
-});
+var sandbox = true;
 
-// 模拟恶意脚本修改页面
-top.document.getElementById('sandbox').innerHTML = 'false';
+try {
+    if (document.cookie) {
+        sandbox = false;
+    }
+} catch(e) {}
+
+try {
+    if (this.document.cookie) {
+        sandbox = false;
+    }
+} catch(e) {}
+
+try {
+    if (window.document.cookie) {
+        sandbox = false;
+    }
+} catch(e) {}
+
+try {
+    if (top.document.cookie) {
+        sandbox = false;
+    }
+} catch(e) {}
+
+try {
+    if (parent.document.cookie) {
+        sandbox = false;
+    }
+} catch(e) {}
+
+try {
+    if (opener.document.cookie) {
+        sandbox = false;
+    }
+} catch(e) {}
+
+try {
+    if (frames[0].document.cookie) {
+        sandbox = false;
+    }
+} catch(e) {}
+
+try {
+    if (self.document.cookie) {
+        sandbox = false;
+    }
+} catch(e) {}
+
+try {
+    top.document.title;
+    sandbox = false;
+} catch(e) {}
+
+// 模拟恶意脚本盗取 cookie
+if (sandbox) {
+    jsonp_callback({
+        name: '糖饼',
+        weibo: 'http://weibo.com/planeart',
+        date: new Date
+    });
+} else {
+    jsonp_callback({
+        sandbox: 'error'
+    });
+    top.document.getElementById('sandbox') = false;
+    throw Error('沙箱失效，cookie 被泄漏');
+}
